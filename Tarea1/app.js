ecfedfa92209db740188dffed7ab5de6b22eb3a6
Felipe Cano <felipecanocarvajal@gmail.com>
2024-09-02 18:37:34 -0500

    Tarea 1

---
+++ Tarea1/app.js
@@ -0,0 +1,16 @@
+const url = 'https://randomuser.me/api/';
+const boton = document.getElementById('boton')
+const contenido = document.querySelector('#contenido');
+
+const mostrarData = async() =>{
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data,'data');
+    contenido.innerHTML = `<img src='${data.results[0].picture.large}'>
+                            <p class="texto1">${data.results[0].cell}</p>
+                            <p class="texto1">${data.results[0].email}</p>
+                            <p class="texto1">${data.results[0].gender}</p>
+                            <p class="texto2">${data.results[0].name.first} ${data.results[0].name.last}</p>`
+}
+
+boton.addEventListener('click', mostrarData);
