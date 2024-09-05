3e71b6b0d2ccf7041a070cf33612607ff1cf072d
Felipe Cano <felipecanocarvajal@gmail.com>
2024-09-05 11:09:05 -0500

    actualizacion Tarea 2

--- Tarea2/src/App.jsx
+++ Tarea2/src/App.jsx
@@ -4,7 +4,7 @@ import CharacterCard from './Components/CharacterCard/CharacterCard'
 function App() {
   return (
       <div>
-        <CharacterCard img='LionelMessi.jpeg' name='Lionel Messi' posicion='Volante Creativo' nacionalidad='Argentina' edad='37 años'></CharacterCard>
+        <CharacterCard img='LionelMessi.jpg' name='Lionel Messi' posicion='Volante Creativo' nacionalidad='Argentina' edad='37 años'></CharacterCard>
         <CharacterCard img='CristianoRonaldo.jpg' name='Cristiano Ronaldo' posicion='Delantero' nacionalidad='Portugal' edad='39 años'></CharacterCard>
         <CharacterCard img='Neymar.jpg' name='Neymar Junior' posicion='Media Punta' nacionalidad='Brasil' edad='32 años'></CharacterCard>
         <CharacterCard img='MarceloViera.jpg' name='Marcelo Viera' posicion='Lateral Izquierdo' nacionalidad='Brasil' edad='36 años'></CharacterCard>
